841a12d5d4e0eefbcb8792b1606f0f4e78a4e003
Bazukova <67784048+Bazukova@users.noreply.github.com>
2021-10-06 18:56:45 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -11,14 +11,14 @@ int main(int argc, char** argv)
 {
 	int height = 520;
 	int width = 840;
-	Mat img(height, width, CV_8UC3);
-	Point textOrg(100, img.rows / 2);
-	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
-	double fontScale = 2;
-	Scalar color(200, 100, 50);
+	Mat img(height, width, CV_8UC3);  //создаётся матрица(Mat) изображение заданной ширены и высоты с 3-х канальным цветом
+	Point textOrg(100, img.rows / 2);  //изменение положения текста по вертикали
+	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX; //настройка шрифта , FONT_HERSHEY_SIMPLEX - тип шрифта
+	double fontScale = 2; 
+	Scalar color(200, 100, 50); //цвет текста
 	putText(img, "OpenCV Step By", textOrg, fontFace, fontScale, color);
 	//namedWindow("Hello World", 0);
-	imshow("Hello World", img);
+	imshow("Hello World", img); //открывает окно
 	waitKey(0);
 	system("pause");
 	return 0;
